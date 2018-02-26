ae60ca52a2f2ae401163967141f2a3b997195f90
fer <fernandoza@gmail.com>
2018-02-26 14:21:14 -0600

    Create the order status enum

---
+++ src/main/java/models/OrderStatus.java
@@ -0,0 +1,25 @@
+package models;
+
+public enum OrderStatus {
+
+    PROCESSING("Processing", 1),
+    SHIPPED("Shipped", 2),
+    DELIVERED("Delivered", 3),
+    CANCELED("Canceled", 4);
+
+    private final String key;
+    private final Integer value;
+
+    OrderStatus(String key, Integer value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return key;
+    }
+    public Integer getValue() {
+        return value;
+    }
+
+}
